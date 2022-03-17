cbff21d281a5d30c93fc953f7c289df878230ce2
Syed Aman <syednashikaman@gmail.com>
2022-03-17 19:19:11 -0400

    Complete unit tests for Content component.

--- src/Content.test.js
+++ src/Content.test.js
@@ -12,8 +12,11 @@
 import React from 'react';
 import { screen, render } from '@testing-library/react';
 import '@testing-library/jest-dom'
+import Chance from 'chance';
 
 import Content from './Content';
+
+const chance = new Chance();
 
 test('Should have heading called "Liked Form Submissions".', () => {
     render(<Content likedForms={[]} />);
@@ -25,3 +28,22 @@ test('An empty array of likedSubmissions should result in no list items.', () =>
     expect(screen.queryByRole('p')).toBeNull();
 });
   
+test('An array of 5 likedSubmissions should result in 5 list items.', () => {
+    const mockFormSubmission = {
+        data: {
+            email: 'testEmail',
+            firstName: 'testFirst',
+            lastName: 'testLast',
+            liked: true
+        }
+    };
+    const mockLikedForms = [...Array(5)].map((elem, i) => ({...mockFormSubmission, id: `testId${i}`}));
+    render(<Content likedForms={mockLikedForms} />);
+    expect(document.getElementsByTagName('p')).toHaveLength(5);
+});
+
+test("List items should be of the form result in '{id} | {firstName} | {lastName}'", () => {
+    const mockLikedForms = [{id:'test',data:{firstName:'t',lastName:'e'}}];
+    render(<Content likedForms={mockLikedForms} />);
+    expect(screen.getByText('test | t | e')).toBeInTheDocument();
+});
