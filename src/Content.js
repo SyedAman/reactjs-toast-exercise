73c7f26c872684e5eb9c04aa62e3c32b1f6bbb83
Syed Aman <syednashikaman@gmail.com>
2022-03-17 09:07:35 -0400

    Update Content component to display a lit of liked submissions.

--- src/Content.js
+++ src/Content.js
@@ -2,14 +2,20 @@ import React from 'react';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 
-export default function Content() {
+function LikedFormSubmissionListItem({props: likedForm}) {
+  return (
+    <Typography variant="body1" sx={{fontStyle: 'italic', marginTop: 1}}>
+      {likedForm.id} | {likedForm.data.firstName} | {likedForm.data.lastName}
+    </Typography>
+  );
+}
+
+export default function Content({props}) {
   return (
     <Box sx={{marginTop: 3}}>
       <Typography variant="h4">Liked Form Submissions</Typography>
 
-      <Typography variant="body1" sx={{fontStyle: 'italic', marginTop: 1}}>
-        TODO: List of liked submissions here (delete this line)
-      </Typography>
+      {props.filter(x => x.data.liked).map(likedForm => <LikedFormSubmissionListItem key={likedForm.id} props={likedForm} />)}
     </Box>
   );
 }
